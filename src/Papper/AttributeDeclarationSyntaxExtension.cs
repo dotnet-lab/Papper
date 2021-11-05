c2055b12514a86bc65e79b00b6509d74568b9787
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2021-11-05 11:01:08 +0800

    Update AttributeDeclarationSyntaxExtension.cs

--- src/Papper/AttributeDeclarationSyntaxExtension.cs
+++ src/Papper/AttributeDeclarationSyntaxExtension.cs
@@ -9,15 +9,29 @@ namespace Microsoft.CodeAnalysis
 
     public static class AttributeDeclarationSyntaxExtension
     {
-
+        /// <summary>
+        /// 获取成员的特性标签语法节点
+        /// </summary>
+        /// <param name="memberDeclarationSyntax"></param>
+        /// <returns></returns>
         public static IEnumerable<AttributeListSyntax> AttributeNodes(this MemberDeclarationSyntax memberDeclarationSyntax)
         {
             return memberDeclarationSyntax.AttributeLists;
         }
+        /// <summary>
+        /// 获取成员的特性标签值
+        /// </summary>
+        /// <param name="memberDeclarationSyntax"></param>
+        /// <returns></returns>
         public static IEnumerable<string> AttributeNames(this MemberDeclarationSyntax memberDeclarationSyntax)
         {
             return memberDeclarationSyntax.AttributeLists.SelectMany(item=>item.Attributes.Select(attr=>attr.Name.ToString()));
         }
+        /// <summary>
+        /// 获取成员的特性整个字符串
+        /// </summary>
+        /// <param name="memberDeclarationSyntax"></param>
+        /// <returns></returns>
         public static string AttributeFullScript(this MemberDeclarationSyntax memberDeclarationSyntax)
         {
             return memberDeclarationSyntax.AttributeLists.ToFullString();
